0acf1dd206dd2bf9f77c057b54c89b70a8cc79e2
sabays <sabay@kth.se>
2025-05-28 08:59:47 +0000

    fixing the paths

--- sim/rtl_sim.sh
+++ sim/rtl_sim.sh
@@ -1,4 +1,10 @@
-bender -d ../src/tb script vsim -t sim > read_src.do
+mkdir temp
+cd temp
+bender -d ../../src/tb script vsim -t sim > read_src.do
+cp ../../sw/bin/instr.bin .
+cp ../../sw/bin/sram_image_in.bin .
+cp ../simulate.do .
+cp ../waves.do .
 vsim -do simulate.do 
 
 
